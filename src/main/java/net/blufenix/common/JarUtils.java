8720dbda9eac55fe515e662d79dddc38c979ca74
BLuFeNiX <blufenix@blufenix.net>
2018-01-16 22:45:04 -0500

    fixed bugs in JarUtils

--- src/main/java/net/blufenix/common/JarUtils.java
+++ src/main/java/net/blufenix/common/JarUtils.java
@@ -1,12 +1,11 @@
 package net.blufenix.common;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
+import net.blufenix.teleportationrunes.TeleportationRunes;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLClassLoader;
@@ -15,109 +14,78 @@ import java.util.Enumeration;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
-import net.blufenix.teleportationrunes.TeleportationRunes;
-
-import org.bukkit.Bukkit;
- 
 public class JarUtils {
-	
-    private static boolean RUNNING_FROM_JAR = false;
-    
-    static {
-        final URL resource = JarUtils.class.getClassLoader().getResource("plugin.yml");
-        if (resource != null) {
-            RUNNING_FROM_JAR = true;
-        }
-    }
- 
-    public static boolean extractFromJar(final String fileName, final String dest) throws IOException {
-        if (getRunningJar() == null) {
-            return false;
-        }
-        final File file = new File(dest);
-        if (file.isDirectory()) {
-            file.mkdir();
-            return false;
-        }
-        if (!file.exists()) {
-            file.getParentFile().mkdirs();
-        }
- 
-        final JarFile jar = getRunningJar();
-        final Enumeration<JarEntry> e = jar.entries();
-        while (e.hasMoreElements()) {
-            final JarEntry je = e.nextElement();
-            if (!je.getName().contains(fileName)) {
-                continue;
-            }
-            final InputStream in = new BufferedInputStream(
-                    jar.getInputStream(je));
-            final OutputStream out = new BufferedOutputStream(
-                    new FileOutputStream(file));
-            copyInputStream(in, out);
-            jar.close();
-            return true;
-        }
-        jar.close();
-        return false;
-    }
- 
-    private final static void copyInputStream(final InputStream in, final OutputStream out) throws IOException {
+
+    /**
+     * Extracts all files from within the running jar's "libs/" directory, placing them in a "libs/" directory in
+     * this plugin's data folder. It then iterates over the files (all assumed to be jars) and adds them to the
+     * classpath of the SystemClassLoader
+     */
+    public static void loadLibs() {
         try {
-            final byte[] buff = new byte[4096];
-            int n;
-            while ((n = in.read(buff)) > 0) {
-                out.write(buff, 0, n);
-            }
-        } finally {
-            out.flush();
-            out.close();
-            in.close();
-        }
-    }
- 
-    public static URL getJarUrl(final File file) throws IOException {
-        return new URL("jar:" + file.toURI().toURL().toExternalForm() + "!/");
-    }
- 
-    public static JarFile getRunningJar() throws IOException {
-        if (!RUNNING_FROM_JAR) {
-            return null; // null if not running from jar
-        }
-        String path = new File(JarUtils.class.getProtectionDomain().getCodeSource().getLocation().getPath()).getAbsolutePath();
-        path = URLDecoder.decode(path, "UTF-8");
-        return new JarFile(path);
-    }
-    
-	public static void loadLibs() {
-    	try {
-            
-        	final File[] libs = new File[] {
-                    new File(TeleportationRunes.getInstance().getDataFolder(), "exp4j.jar")
-            };
-            
-            for (final File lib : libs) {
-                if (!lib.exists()) {
-                    JarUtils.extractFromJar(lib.getName(), lib.getAbsolutePath());
-                }
-            }
-            
-            for (final File lib : libs) {
-                if (!lib.exists()) {
-                	TeleportationRunes.getInstance().getLogger().warning("There was a critical error loading TeleportationRunes!" +
-                    		" Could not find lib: " + lib.getName());
-                    Bukkit.getServer().getPluginManager().disablePlugin(TeleportationRunes.getInstance());
-                    return;
-                }
+            String dataFolder = TeleportationRunes.getInstance().getDataFolder().getAbsolutePath();
+            extractDirFromJar("libs/", dataFolder);
+            for (final File lib : new File(dataFolder+"/libs").listFiles()) {
                 addClassPath(JarUtils.getJarUrl(lib));
             }
-            
-        } catch (final Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
-	}
+    }
 
-	private static void addClassPath(final URL url) throws IOException {
+    /**
+     *
+     * Copies sourceDir (from JAR) into parentDestDir (on local filesystem)
+     */
+    private static boolean extractDirFromJar(String sourceDir, String parentDestDir) {
+        try {
+            if (!sourceDir.endsWith("/")) sourceDir += "/";
+
+            JarFile jar = getRunningJar();
+            Enumeration<JarEntry> enumEntries = jar.entries();
+            while (enumEntries.hasMoreElements()) {
+                JarEntry entry = enumEntries.nextElement();
+                String entryName = entry.getName();
+
+                if (!entryName.startsWith(sourceDir)) continue;
+
+                File f = new File(parentDestDir + java.io.File.separator + entry.getName());
+                if (entry.isDirectory()) { // if it's a directory, create it
+                    f.mkdirs();
+                    continue;
+                }
+                InputStream is = jar.getInputStream(entry); // get the input stream
+                FileOutputStream fos = new FileOutputStream(f);
+                while (is.available() > 0) {
+                    fos.write(is.read()); // write contents of 'is' to 'fos'
+                }
+                fos.close();
+                is.close();
+            }
+            jar.close();
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private static JarFile getRunningJar() {
+        try {
+            String path = new File(JarUtils.class.getProtectionDomain().getCodeSource().getLocation().getPath()).getAbsolutePath();
+            path = URLDecoder.decode(path, "UTF-8");
+            return new JarFile(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private static URL getJarUrl(final File file) throws IOException {
+        return new URL("jar:" + file.toURI().toURL().toExternalForm() + "!/");
+    }
+
+    private static void addClassPath(final URL url) throws IOException {
         final URLClassLoader sysloader = (URLClassLoader) ClassLoader.getSystemClassLoader();
         final Class<URLClassLoader> sysclass = URLClassLoader.class;
         try {
@@ -129,5 +97,5 @@ public class JarUtils {
             throw new IOException("Error adding " + url + " to system classloader");
         }
     }
- 
+
 }
